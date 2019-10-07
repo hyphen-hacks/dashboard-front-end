53dc129ffd04bc02a49b72a19b46098504dd3d43
Ronan <rsf.sho@gmail.com>
2019-10-06 19:11:10 -0700

    calender wip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,14 +10,11 @@
       </div>
 
     </div>
-    <div v-if="false" class="send-notification notReady card--light">
-      <h1 class="card__heading">Send Notification</h1>
-      <form @submit.prevent>
-        <input disabled placeholder="title" type="text">
-        <textarea disabled placeholder="message"></textarea>
-        <button disabled type="submit">SEND</button>
-      </form>
+    <div class="card--light">
+      <h1 class="card__heading">Event Analytics</h1>
+      <canvas id="eventAnalyticsChart"></canvas>
     </div>
+    <br>
     <div class="stats card--light">
       <h1 class="card__heading">Stats <span class="secondaryHeading">Updated {{updatedTime}}</span></h1>
       <chart v-for="dataSet in chartData" :input="dataSet" :key="dataSet.title"></chart>
@@ -216,7 +213,178 @@
         })
 
       })
+      let eventAnalyticsChart;
+      this.$firebase.firestore().collection('eventAnalytics').onSnapshot(snap => {
+        let analyticsData = {
+          labels: [],
+          datasets: [{
+            data: [],
+            label: 'Attendees',
+            borderColor: "#3e95cd",
+            fill: false,
+            yAxisID: "B"
+          },
+            {
+              data: [],
+              label: 'Attendees On Campus',
+              borderColor: "#8e5ea2",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Attendees Checked In',
+              borderColor: "#3cba9f",
+              fill: false,
+              yAxisID: "A"
+            }, {
+              data: [],
+              label: 'Mentors',
+              borderColor: "#1abc9c",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Mentors On Campus',
+              borderColor: "#2ecc71",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Mentors Checked In',
+              borderColor: "#3498db",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Volunteers',
+              borderColor: "#3e95cd",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Volunteers On Campus',
+              borderColor: "#9b59b6",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Volunteers Checked In',
+              borderColor: "#e74c3c",
+              fill: false,
+              yAxisID: "A"
+            },
+            {
+              data: [],
+              label: 'Waivers Completed',
+              borderColor: "#e67e22",
+              fill: false,
+              yAxisID: "A"
+            }]
+        }
+        snap.forEach(time => {
+          //analyticsData.push(time.data())
+          const data = time.data()
+         // console.log(data.timeStamp, moment.unix(data.timeStamp).toObject(), new Date(data.timeStamp).toLocaleString())
+          analyticsData.labels.push(moment.unix(data.timeStamp).format('dddd, MMMM Do YYYY, h:mm:ss a'))
+          //const timeToUse = moment.unix(data.timeStamp).toDate()
+          const timeToUse = new Date(data.timeStamp)
+          console.log(timeToUse, 'time to use')
+          analyticsData.datasets[0].data.push({t: timeToUse, y: data.attendees.total})
+          //analyticsData.datasets[1].data.push({t: timeToUse, y: data.attendees.onCampus})
+          //analyticsData.datasets[2].data.push({t: timeToUse, y: data.attendees.checkedIn})
+         // analyticsData.datasets[3].data.push({t: timeToUse, y: data.mentors.total})
+         // analyticsData.datasets[4].data.push({t: timeToUse, y: data.mentors.onCampus})
+         // analyticsData.datasets[5].data.push({t: timeToUse, y: data.mentors.checkedIn})
+         // analyticsData.datasets[6].data.push({t: timeToUse, y: data.volunteers.total})
+         /// analyticsData.datasets[7].data.push({t: timeToUse, y: data.volunteers.onCampus})
+        ///  analyticsData.datasets[8].data.push({t: timeToUse, y: data.volunteers.checkedIn})
+        //  analyticsData.datasets[9].data.push({
+          //  t: timeToUse,
+          //  y: (data.attendees.waiversComplete + data.volunteers.waiversComplete + data.mentors.waiversComplete)
+         // })
+          /*analyticsData.datasets[0].data.push({x: data.timeStamp, y: data.attendees.total})
+          analyticsData.datasets[1].data.push({x: data.timeStamp, y: data.attendees.onCampus})
+          analyticsData.datasets[2].data.push({x: data.timeStamp, y: data.attendees.checkedIn})
+          analyticsData.datasets[3].data.push({x: data.timeStamp, y: data.mentors.total})
+          analyticsData.datasets[4].data.push({x: data.timeStamp, y: data.mentors.onCampus})
+          analyticsData.datasets[5].data.push({x: data.timeStamp, y: data.mentors.checkedIn})
+          analyticsData.datasets[6].data.push({x: data.timeStamp, y: data.volunteers.total})
+          analyticsData.datasets[7].data.push({x: data.timeStamp, y: data.volunteers.onCampus})
+          analyticsData.datasets[8].data.push({x: data.timeStamp, y: data.volunteers.checkedIn})
+          analyticsData.datasets[9].data.push({
+            x: data.timeStamp,
+            y: (data.attendees.waiversComplete + data.volunteers.waiversComplete + data.mentors.waiversComplete)
+          })*/
 
+        })
+        console.log(analyticsData)
+        const ctx = document.getElementById('eventAnalyticsChart').getContext('2d');
+        eventAnalyticsChart = new Chart(ctx, {
+          type: 'line',
+          data: analyticsData  /*{
+            labels: [1500, 1600, 1700, 1750, 1800, 1850, 1900, 1950, 1999, 2050],
+            datasets: [{
+              data: [86, 114, 106, 106, 107, 111, 133, 221, 783, 2478],
+              label: "Africa",
+              borderColor: "#3e95cd",
+              fill: false
+            }, {
+              data: [282, 350, 411, 502, 635, 809, 947, 1402, 3700, 5267],
+              label: "Asia",
+              borderColor: "#8e5ea2",
+              fill: false
+            }, {
+              data: [168, 170, 178, 190, 203, 276, 408, 547, 675, 734],
+              label: "Europe",
+              borderColor: "#3cba9f",
+              fill: false
+            }, {
+              data: [40, 20, 10, 16, 24, 38, 74, 167, 508, 784],
+              label: "Latin America",
+              borderColor: "#e8c3b9",
+              fill: false
+            }, {
+              data: [6, 3, 2, 2, 7, 26, 82, 172, 312, 433],
+              label: "North America",
+              borderColor: "#c45850",
+              fill: false
+            }
+            ]
+          }*/,
+          options: {
+            scales: {
+              xAxes: [{
+                type: 'time',
+                time: {
+                //  unit: 'hour'
+                }
+              }],
+              yAxes: [{
+                id: 'A',
+                type: 'linear',
+                position: 'left',
+              }, {
+                id: 'B',
+                type: 'linear',
+                position: 'right',
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            },
+            title: {
+              display: false,
+              text: 'P'
+            }
+          }
+        })
+      })
 
     },
     methods: {}
