b895c17de67fc1ba055f33f80fef58ca9d705a82
Ronan <rsf.sho@gmail.com>
2019-10-06 21:32:27 -0700

    started schedule development

--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -1,7 +1,42 @@
 <template>
-  <main class="developer">
+  <main class="roster">
     <loader v-if="loadingData"></loader>
+    <div class="roster__people">
+      <nav class="people__nav">
 
+        <div class="items"><p class="nav__name">Title</p>
+          <p class="nav__time">Start</p>
+          <p class="nav__time">End</p>
+          <p class="nav__location">Location</p>
+        </div>
+
+      </nav>
+      <div v-for="item in [1,2,3,4,5]"
+           :class="{chosen:false}" class="person">
+        <p class="person__name">Opening Ceremony</p>
+        <p class="person__time">Sat, 10:00am</p>
+        <p class="person__time">Sat, 11:00am</p>
+        <p class="person__location">Gym</p>
+
+      </div>
+    </div>
+    <div class="roster__info">
+      <div class="info__nameRow">
+        <div class="nameRow__nameContainer">
+          <h1 class="nameRow__name">Opening Ceremony</h1>
+
+          <p class="nameRow__school">Sat, 10:00am to Sat, 11:00am in Gym</p>
+
+
+        </div>
+
+      </div>
+
+
+    </div>
+    <div v-if="!true" class="roster__info help">
+      <p class="helpText">Click on someone from the roster to get more information about them</p>
+    </div>
   </main>
 </template>
 
