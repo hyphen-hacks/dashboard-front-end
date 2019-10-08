e1e9b1a92b44ebd33901c9973625db63406dacbe
Ronan <rsf.sho@gmail.com>
2019-10-07 20:57:33 -0700

    fleshing out schedule

--- src/views/schedule.vue
+++ src/views/schedule.vue
@@ -4,8 +4,9 @@
     <div class="roster__people">
       <nav class="people__nav">
 
-        <div class="items"><p class="nav__name">Title</p>
-          <p class="nav__time">Start</p>
+        <div class="items">
+          <p class="nav__name">Title</p>
+          <p class="nav__time start">Start</p>
           <p class="nav__time">End</p>
           <p class="nav__location">Location</p>
         </div>
@@ -14,7 +15,7 @@
       <div v-for="item in [1,2,3,4,5]"
            :class="{chosen:false}" class="person">
         <p class="person__name">Opening Ceremony</p>
-        <p class="person__time">Sat, 10:00am</p>
+        <p class="person__time start">Sat, 10:00am</p>
         <p class="person__time">Sat, 11:00am</p>
         <p class="person__location">Gym</p>
 
@@ -31,7 +32,24 @@
         </div>
 
       </div>
-
+      <br>
+      <form class="scheduleDetails" @submit.prevent>
+        <label for="scheduleTitle">Title</label>
+        <input id="scheduleTitle" type="text" placeholder="Title">
+        <label for="scheduleStart">Start Time (ex. 2019-10-12 09:30) <span class="green">Valid</span></label>
+        <input id="scheduleStart" type="text" placeholder="Start Time (ex. 2019-10-12 09:30)">
+        <label for="scheduleEnd">End Time (ex. 2019-10-12 09:30) <span class="green">Valid</span></label>
+        <input id="scheduleEnd" type="text" placeholder="End Time (ex. 2019-10-12 09:30)">
+        <div class="row">
+          <label for="scheduleLocation">Title</label>
+          <input id="scheduleLocation" type="text" placeholder="Location">
+          <label for="scheduleType">Title</label>
+          <input id="scheduleType" type="text" placeholder="Type">
+        </div>
+        <label for="scheduleDesc">Description</label>
+        <textarea id="scheduleDesc" placeholder="Description"></textarea>
+        <button type="submit" class="btn">Save</button>
+      </form>
 
     </div>
     <div v-if="!true" class="roster__info help">
